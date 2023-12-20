5a01d0d45878637bb931d29251013492ed12dcd3
charline-studi <studi@charline-laporte.fr>
2023-12-20 11:12:17 +0100

    Fin du cours #5

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,28 @@
 - Cliquer sur une case de mon calendrier
 - Cette case doit avoir un numéro inférieur à la date du jour
 - Si elle ne l'est pas il ne se passe rien
-- Si elle l'a la suite se passe
+- Si elle l'a la suite se passe :
 - Jouer de la musique
 - Afficher l'image de fond de la case
+- Affiche une popup avec une citation
+- Quand on ferme la popup on arrête la musique
+- Sauvegarder un historique des cases cliquées dans le navigateur
 */
-
 import quotes from "./quotes.js"
+
+const showPreviouslyOpenedBoxes = () => {
+    // 1 - Récupérer les nombres de boxes déjà ouvertes
+    const openedBoxes = localStorage.getItem("openedBoxes")
+    if (openedBoxes !== null) {
+        // 2 - 'Ouvrir' visuellement ces boxes
+        const listOfBoxes = openedBoxes.split(",")
+        for(let i = 0; i < listOfBoxes.length; i++) {
+            const boxNumber = listOfBoxes[i]
+            const box = document.querySelector(`[data-number="${boxNumber}"]`)
+            showImage(box)
+        }
+    }
+}
 
 const boxes = document.querySelectorAll(".js-box")
 const today = new Date(Date.now())
@@ -20,6 +36,7 @@ boxes.forEach(box => {
             playSong()
             showImage(box)
             openModal(boxNumber)
+            saveHistory(boxNumber)
         }
     })
 });
@@ -44,3 +61,20 @@ const openModal = (index) => {
     author.textContent = quotes[index].author
     modal.showModal()
 }
+
+modal.addEventListener('close', () => {
+    song.pause()
+})
+
+const saveHistory = (boxNumber) => {
+    let openedBoxes = []
+    let localValue = localStorage.getItem("openedBoxes")
+    if (localValue !== null) {
+        openedBoxes = localValue.split(',')
+    }
+    openedBoxes.push(boxNumber)
+    localStorage.setItem("openedBoxes", openedBoxes)
+}
+
+
+showPreviouslyOpenedBoxes()
